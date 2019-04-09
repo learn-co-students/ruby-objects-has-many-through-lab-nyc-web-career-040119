c93018bc5b7a64a79a87b46c80f7270f617a1f76
ferrisboran <ferris@ferryrules.com>
2019-04-08 20:04:44 -0400

    Done.

---
+++ lib/patient.rb
@@ -0,0 +1,14 @@
+class Patient < Doctor
+
+  def new_appointment(doctor, date)
+    Appointment.new(self, doctor, date)
+  end
+
+  def appointments
+    Appointment.all.select { |appointment| appointment.patient == self }
+  end
+
+  def doctors
+    appointments.map { |appointment| appointment.doctor }
+  end
+end
